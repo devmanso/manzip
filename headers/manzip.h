24da896c708790cf188b3307f7c5d528e66bfbb8
Mansour Quddus <itsnothisuser@gmail.com>
2023-10-04 23:17:56 -0400

    more testing

--- headers/manzip.h
+++ headers/manzip.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 void performRLECompression(FILE *inputFile, FILE *outputFile) {
     int currentChar, lastChar;
@@ -69,3 +70,66 @@ void performRLEDecompression(FILE *inputFile, FILE *outputFile) {
     //fclose(outputFile);
 }
 
+
+/* Returns the Run Length Encoded string for the 
+   source string src */
+char* encodeString(char* src, int max_rlen)
+{
+    int rLen;
+    char count[max_rlen];
+    int len = strlen(src);
+ 
+    /* If all characters in the source string are different, 
+    then size of destination string would be twice of input string.
+    For example if the src is "abcd", then dest would be "a1b1c1d1"
+    For other inputs, size would be less than twice.  */
+    char* dest = (char*)malloc(sizeof(char) * (len * 2 + 1));
+ 
+    int i, j = 0, k;
+ 
+    /* traverse the input string one by one */
+    for (i = 0; i < len; i++) {
+ 
+        /* Copy the first occurrence of the new character */
+        dest[j++] = src[i];
+ 
+        /* Count the number of occurrences of the new character */
+        rLen = 1;
+        while (i + 1 < len && src[i] == src[i + 1]) {
+            rLen++;
+            i++;
+        }
+ 
+        /* Store rLen in a character array count[] */
+        sprintf(count, "%d", rLen);
+ 
+        /* Copy the count[] to destination */
+        for (k = 0; *(count + k); k++, j++) {
+            dest[j] = count[k];
+        }
+    }
+ 
+    /*terminate the destination string */
+    dest[j] = '\0';
+    return dest;
+}
+
+void encodeFile(FILE* inputFile, FILE* outputFile, int max_rlen) {
+    // Buffer to read each line from the file
+    char buffer[1024];
+
+    // Read lines from the input file
+    while (fgets(buffer, sizeof(buffer), inputFile) != NULL) {
+        // Remove newline character from the end of the line
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        // Encode the line using encodeString
+        char* encodedLine = encodeString(buffer, max_rlen);
+
+        // Write the encoded line to the output file
+        fprintf(outputFile, "%s\n", encodedLine);
+
+        // Free the memory allocated by encodeString
+        free(encodedLine);
+    }
+}
--- main.c
+++ main.c
@@ -4,29 +4,53 @@
 
 int main() {
     FILE* inputFile = fopen("input.wasd", "r");
+
     if(!inputFile) {
         perror("could not open input file");
+        fclose(inputFile);
         return EXIT_FAILURE;
     }
 
-    // Open the output file for writing
-    FILE *outputFile = fopen("output.wasd", "w");
-    if (!outputFile) {
-        perror("Error opening output file");
-        fclose(inputFile);
-        return 1;
+    FILE* outputFile = fopen("output.wasd", "w");
+
+    if(!outputFile) {
+        perror("could not open output file");
+        fclose(outputFile);
+        return EXIT_FAILURE;
     }
-    performRLECompression(inputFile, outputFile);
-    
+
+    int max_rlen = 9;
+
+    encodeFile(inputFile, outputFile, max_rlen);
+
     fclose(inputFile);
     fclose(outputFile);
+    printf("Success");
 
-    FILE* compressedFile = fopen("output.wasd", "r");
-    FILE* ogFile = fopen("og.wasd", "w");
+    // FILE* inputFile = fopen("input.wasd", "rb");
+    // if(!inputFile) {
+    //     perror("could not open input file");
+    //     return EXIT_FAILURE;
+    // }
 
-    performRLEDecompression(compressedFile, ogFile);
-    fclose(compressedFile);
-    fclose(ogFile);
+    // // Open the output file for writing
+    // FILE *outputFile = fopen("output.wasd", "wb");
+    // if (!outputFile) {
+    //     perror("Error opening output file");
+    //     fclose(inputFile);
+    //     return 1;
+    // }
+    // performRLECompression(inputFile, outputFile);
+    
+    // fclose(inputFile);
+    // fclose(outputFile);
+
+    // FILE* compressedFile = fopen("output.wasd", "r");
+    // FILE* ogFile = fopen("og.wasd", "w");
+
+    // performRLEDecompression(compressedFile, ogFile);
+    // fclose(compressedFile);
+    // fclose(ogFile);
     getchar();
     return EXIT_SUCCESS;
 }
