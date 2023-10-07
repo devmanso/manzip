b863dd489b5437a0e3cc5eb5b7e998c68ebd8d41
Mansour Quddus <itsnothisuser@gmail.com>
2023-10-07 15:32:57 -0400

    README and initial version of manzip (1.0)

---
+++ README.md
@@ -0,0 +1,45 @@
+# manzip
+
+* manzip is a file comrpession program that I've been working on to learn about file compression algorithms.
+
+* This program only works with text files written in english and is ASCII text. Something like "صباح الخير" will most likely not get encoded, or something wierd will happen.
+
+* This is liscensed under MIT Liscense, I am not responsible for any damages to your data or system.
+
+## Under-The-Hood
+
+The way this program encodes/decodes files is through run-length-encoding. This means that sometimes file sizes might actually increase when encoded ("abcd" turns into "a1b1c1d1" when encoded).
+RLE works best when there is a lot of repetition (i.e: "aaabbbcccddd" turns into "a3b3c3d3" when encoded)
+
+### The code
+
+[main.c](main.c): This is the entry point of the program and is the CLI, it takes 3 arguments representing the input/output file and encoding/decoding. It handles misinputs and errors. It requires manzip.h to work
+[manzip.h](headers/manzip.h): This is where the RLE algorithm is implemented, it has encoding/decoding functions for both files and strings. You can inlude this header file into your own projects too, it only depends on the standard library
+
+### How to compile (C99)
+
+```
+ gcc -o manzip -I./headers main.c
+```
+
+## How to use
+
+Usage is also very simple, the program just needs 3 parameters
+
+* input file ("myFile.ext")
+
+* output file ("myEncodedFile.ext")
+
+* mode ("E" or "D")
+
+* Example usage (Encoding)
+
+```
+./manzip input.txt output.txt E
+```
+
+* Example usage (Decoding)
+
+```
+./manzip encoded.txt decoded.txt D
+```
--- main.c
+++ main.c
@@ -1,71 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 #include "headers/manzip.h"
 
-int main() {
-    // FILE* inputFile = fopen("input.wasd", "r");
+int main(int argc, char *argv[]) {
 
-    // if(!inputFile) {
-    //     perror("could not open input file");
-    //     fclose(inputFile);
-    //     return EXIT_FAILURE;
-    // }
+    if(argc != 4) {
+        perror("Incorrect number of arguments! There should be 3 arguments: \n");
+        perror("input_file.ext output_file.ext mode \n");
+        perror("where mode can either be E for encoding, or D for decoding");
+        return EXIT_FAILURE;
+    }
 
-    // FILE* outputFile = fopen("output.wasd", "w");
+    // inputs
+    const char* inputFilepath = argv[1];
+    const char* outputFilepath = argv[2];
+    const char* mode = argv[3];
 
-    // if(!outputFile) {
-    //     perror("could not open output file");
-    //     fclose(outputFile);
-    //     return EXIT_FAILURE;
-    // }
+    if (access(inputFilepath, F_OK) == -1) {
+        fprintf(stderr, "Error: File '%s' does not exist.\n", inputFilepath);
+        return EXIT_FAILURE;
+    }
 
-    // int max_rlen = 9;
+    FILE* inputFile = fopen(inputFilepath, "r");
+    if(inputFile == NULL) {
+        perror("Could not open input file");
+        fclose(inputFile);
+        return EXIT_FAILURE;
+    }
 
-    // encodeFile(inputFile, outputFile, max_rlen);
+    FILE* outputFile = fopen(outputFilepath, "w");
+    if(outputFile == NULL) {
+        perror("Could not open output file");
+        fclose(outputFile);
+        return EXIT_FAILURE;
+    }
 
-    // fclose(inputFile);
-    // fclose(outputFile);
-    // printf("Success");
+    if(strcmp(mode, "E") != 0 && strcmp(mode, "D") != 0) {
+        perror("incorrect mode: mode must be E or D");
+        return EXIT_FAILURE;
+    }
 
-    FILE* reinput = fopen("output.wasd", "r");
-    FILE* reoutput = fopen("reconstruct.wasd", "w");
-    decodeFile(reinput, reoutput);
-    printf("done");
-
-    fclose(reinput);
-    fclose(reoutput);
-
-    // while(1==1) {
-    //     char buff[1024];
-    //     int input = scanf("%s", buff);
-    //     char* decodedString = decodeString(buff);
-    //     printf("%s \n", decodeString(buff));
-    // }
-
-    // FILE* inputFile = fopen("input.wasd", "rb");
-    // if(!inputFile) {
-    //     perror("could not open input file");
-    //     return EXIT_FAILURE;
-    // }
-
-    // // Open the output file for writing
-    // FILE *outputFile = fopen("output.wasd", "wb");
-    // if (!outputFile) {
-    //     perror("Error opening output file");
-    //     fclose(inputFile);
-    //     return 1;
-    // }
-    // performRLECompression(inputFile, outputFile);
+    if(strcmp(mode, "E") == 0) {
+        encodeFile(inputFile, outputFile, 9);
+        fclose(inputFile);
+        fclose(outputFile);
+        return EXIT_SUCCESS;
+    } else if(strcmp(mode, "D") == 0) {
+        decodeFile(inputFile, outputFile);
+        fclose(inputFile);
+        fclose(outputFile);
+        return EXIT_SUCCESS;
+    }
     
-    // fclose(inputFile);
-    // fclose(outputFile);
-
-    // FILE* compressedFile = fopen("output.wasd", "r");
-    // FILE* ogFile = fopen("og.wasd", "w");
-
-    // performRLEDecompression(compressedFile, ogFile);
-    // fclose(compressedFile);
-    // fclose(ogFile);
+    printf("\n Operation done, press any key to exit");
+    
     getchar();
     return EXIT_SUCCESS;
 }
